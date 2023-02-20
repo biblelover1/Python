40d931704f477490d087be2120d89941bbc0a483
Evgeniy <evgenijkopylov19540@gmail.com>
2023-02-20 22:27:27 +0300

    Errors fixed

--- Task_1.py
+++ Task_1.py
@@ -2,10 +2,6 @@
 # *Пример:*
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0) 
-# chislo = input("Введите трехзначное число: ")
-# sum = 0
-# for i in range(len(chislo)):
-#     s = chislo[i]
-#     s = int(s)
-#     sum += s
-# print(sum)
+number = input("Введите трехзначное число: ")
+sum = 0
+print(f"{int(number[0]) + int(number[1]) + int(number[2])}")
